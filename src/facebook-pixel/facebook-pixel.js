254afba74df2d21415600a3ce69e93ba96383963
Ignacio Santolin <isantolin@users.noreply.github.com>
2017-05-28 18:29:14 -0300

    Corrected code

--- src/facebook-pixel/facebook-pixel.js
+++ src/facebook-pixel/facebook-pixel.js
@@ -9,51 +9,47 @@
 // Feature configuration
 
 Object.assign(mejs.MepDefaults, {
-	/**
-  * @type {String}
-  */
-	facebookPixelTitle: '',
-	/**
-  * @type {String}
-  */
-	facebookPixelCategory: 'Videos',
-	/**
-  * @type {String}
-  */
-	facebookPixelEventTime: 'Time'
+    /**
+     * @type {String}
+     */
+    facebookPixelTitle: '',
+    /**
+     * @type {String}
+     */
+    facebookPixelCategory: 'Videos',
+    /**
+     * @type {String}
+     */
+    facebookPixelEventTime: 'Time'
 });
 
 Object.assign(MediaElementPlayer.prototype, {
 
-	/**
-  * Feature constructor.
-  *
-  * Always has to be prefixed with `build` and the name that will be used in MepDefaults.features list
-  * @param {MediaElementPlayer} player
-  * @param {HTMLElement} controls
-  * @param {HTMLElement} layers
-  * @param {HTMLElement} media
-  */
-	buildfacebookpixel: function buildfacebookpixel(player, controls, layers, media) {
+    /**
+     * Feature constructor.
+     *
+     * Always has to be prefixed with `build` and the name that will be used in MepDefaults.features list
+     * @param {MediaElementPlayer} player
+     * @param {HTMLElement} controls
+     * @param {HTMLElement} layers
+     * @param {HTMLElement} media
+     */
+    buildfacebookpixel: function buildfacebookpixel(player, controls, layers, media) {
 
-		media.addEventListener('play', function () {
-			if (typeof fbq !== 'undefined') {
-				fbq('trackCustom', player.options.facebookPixelCategory, {Event: 'Play', Title: player.options.facebookPixelTitle === '' ? player.media.currentSrc : player.options.facebookPixelTitle});
-			}
-		}, false);
-
-		media.addEventListener('pause', function () {
-			if (typeof fbq !== 'undefined') {
-				fbq('trackCustom', player.options.facebookPixelCategory, {Event: 'Pause', Title: player.options.facebookPixelTitle === '' ? player.media.currentSrc : player.options.facebookPixelTitle});
-			}
-		}, false);
-
-		media.addEventListener('ended', function () {
-			if (typeof fbq !== 'undefined') {
-				fbq('trackCustom', player.options.facebookPixelCategory, {Event: 'Ended', Title: player.options.facebookPixelTitle === '' ? player.media.currentSrc : player.options.facebookPixelTitle});
-			}
-		}, false);
-	}
+        media.addEventListener('play', function () {
+            if (typeof fbq !== 'undefined') {
+                fbq('trackCustom', player.options.facebookPixelCategory, {Event: 'Play', Title: player.options.facebookPixelTitle === '' ? player.media.currentSrc : player.options.facebookPixelTitle});
+            }
+        }, false);
+        media.addEventListener('pause', function () {
+            if (typeof fbq !== 'undefined') {
+                fbq('trackCustom', player.options.facebookPixelCategory, {Event: 'Pause', Title: player.options.facebookPixelTitle === '' ? player.media.currentSrc : player.options.facebookPixelTitle});
+            }
+        }, false);
+        media.addEventListener('ended', function () {
+            if (typeof fbq !== 'undefined') {
+                fbq('trackCustom', player.options.facebookPixelCategory, {Event: 'Ended', Title: player.options.facebookPixelTitle === '' ? player.media.currentSrc : player.options.facebookPixelTitle});
+            }
+        }, false);
+    }
 });
-
-},{}]},{},[1]);
