e100729181ed63d6bbf1d0e339b51739ad27b3ff
Jeff Hutchins <complife@gmail.com>
2017-05-29 14:40:52 -0600

    Good layout

--- src/lock/lock.js
+++ src/lock/lock.js
@@ -38,21 +38,17 @@ Object.assign(MediaElementPlayer.prototype, {
 	 * Feature constructor.
 	 *
 	 * Always has to be prefixed with `build` and the name that will be used in MepDefaults.features list
+	 * @param {MediaElementPlayer} player
+	 * @param {$} controls
+	 * @param {$} layers
 	 */
-	buildlock ()  {
+	buildlock (player, controls, layers)  {
 		const
 			t = this,
 			lockText = mejs.Utils.isString(t.options.lockText) ? t.options.lockText : mejs.i18n.t('mejs.lock'),
-			unlockText = mejs.Utils.isString(t.options.unlockText) ? t.options.unlockText : mejs.i18n.t('mejs.unlock'),
-			lock = document.createElement('div')
+			unlockText = mejs.Utils.isString(t.options.unlockText) ? t.options.unlockText : mejs.i18n.t('mejs.unlock')
 		;
 		let locked = false;
-
-		lock.className = `${t.options.classPrefix}button ${t.options.classPrefix}lock-button`;
-		lock.innerHTML = `<button type="button" aria-controls="${t.id}" title="${lockText}" aria-label="${lockText}" tabindex="0"></button>`;
-
-		t.addControlElement(lock, 'lock');
-		window.player = t;
 
 		Object.defineProperty(t, 'locked', {
 			get: function() {
@@ -63,18 +59,42 @@ Object.assign(MediaElementPlayer.prototype, {
 				locked = value;
 				if (locked) {
 					t.options.clickToPlayPause = false;
+					t.options.disableControls = true;
 					t.controls.style.display = 'none';
+					t.unlockButton.style.display = '';
 				} else {
 					t.options.clickToPlayPause = true;
+					t.options.disableControls = false;
 					t.controls.style.display = '';
+					t.unlockButton.style.display = 'none';
 				}
 			}
 		});
 
+		window.player = t;
+		window.layers = layers;
+
+		// Unlock button
+		player.unlockButton = document.createElement('div');
+		player.unlockButton.className = `${t.options.classPrefix}layer ${t.options.classPrefix}unlock-button`;
+		player.unlockButton.innerHTML = `<button type="button" title="${unlockText}" aria-label="${unlockText}" tabindex="0"></button>`;
+		player.unlockButton.style.display = 'none';
+		layers.insertBefore(player.unlockButton, layers.firstChild);
+
+		player.unlockButton.addEventListener('click', () => {
+			console.log(unlockText);
+		});
+
+		// Lock button
+		player.lockButton = document.createElement('div');
+		player.lockButton.className = `${t.options.classPrefix}button ${t.options.classPrefix}lock-button`;
+		player.lockButton.innerHTML = `<button type="button" aria-controls="${t.id}" title="${lockText}" aria-label="${lockText}" tabindex="0"></button>`;
+		t.addControlElement(player.lockButton, 'lock');
+
 		// add a click toggle event
-		lock.addEventListener('click', () => {
+		player.lockButton.addEventListener('click', () => {
 			t.lock();
-			console.log(unlockText )
+			console.log(lockText);
 		});
 	},
 
