3bf96a516802ff8a9410040d2214d08722ac6ed0
Muhammad Nur Hidayat Yasuyoshi (MNH48.com) <muhdnurhidayat96@yahoo.com>
2018-01-28 00:54:50 +0800

    Add Malay translation

--- src/postroll/postroll-i18n.js
+++ src/postroll/postroll-i18n.js
@@ -33,6 +33,9 @@ if (mejs.i18n.ja !== undefined) {
 if (mejs.i18n.ko !== undefined) {
 	mejs.i18n.ko['mejs.close'] = '종료';
 }
+if (mejs.i18n.ms !== undefined) {
+	mejs.i18n.ms['mejs.close'] = 'Tutup';
+}
 if (mejs.i18n.nl !== undefined) {
 	mejs.i18n.nl['mejs.close'] = 'Sluiten';
 }
