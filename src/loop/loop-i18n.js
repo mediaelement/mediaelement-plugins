4639e30f6874bf8959ef14a4595ae7542c570d64
Muhammad Nur Hidayat Yasuyoshi (MNH48.com) <muhdnurhidayat96@yahoo.com>
2018-01-28 00:51:01 +0800

    Add Malay translation

--- src/loop/loop-i18n.js
+++ src/loop/loop-i18n.js
@@ -33,6 +33,9 @@ if (mejs.i18n.ja !== undefined) {
 if (mejs.i18n.ko !== undefined) {
 	mejs.i18n.ko['mejs.loop'] = '루프 토글';
 }
+if (mejs.i18n.ms !== undefined) {
+	mejs.i18n.ms['mejs.loop'] = 'Togol mod ulangan video';
+}
 if (mejs.i18n.nl !== undefined) {
 	mejs.i18n.nl['mejs.loop'] = 'Schakellus';
 }
