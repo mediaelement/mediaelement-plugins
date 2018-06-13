ca8f725e1ffc8a8b178dc39343bbedc528402799
Hakim Rachidi <hakimrachidi02@gmail.com>
2018-06-14 00:17:46 +0200

    Add picture-in-picture indenting / docs fix

--- src/picture-in-picture/picture-in-picture.js
+++ src/picture-in-picture/picture-in-picture.js
@@ -16,18 +16,18 @@ mejs.i18n.en['mejs.picture-in-pictureText'] = "Picture in Picture";
 // Feature configuration
 Object.assign(mejs.MepDefaults, {
     /**
-	  * @type {Bool}
+	  * @type {Bool} standartScaleEnd - Rezooms the video back into the video elment at the End
 	  */
 	  standartScaleEnd: true,
 
      /**
-	  * @type {Bool}
+	  * @type {Bool} picInPicScaleStart - Makes it possible to start the video in PiP-mode
 	  */
 	  picInPicScaleStart: false,
 
 
     /**
-	  * @type {?String}
+	  * @type {?String} picInPicTitle - Sets the hovertitle of the PiP button
 	  */
 	  picInPicTitle: null
 });
@@ -45,53 +45,51 @@ Object.assign(MediaElementPlayer.prototype, {
      */
     buildpictureInPicture (player, controls, layers, media) {
 
-        const
-          t = this,
-          buttonWrapper = document.createElement('div'),
-          button = document.createElement('button'),
-					video = t.node
-        ;
+      const
+        t = this,
+        buttonWrapper = document.createElement('div'),
+        button = document.createElement('button'),
+				video = t.node
+      ;
 
-        if(video instanceof HTMLVideoElement) {
+      if(video instanceof HTMLVideoElement) {
 
-		// define inner button attributes
-	       	button.setAttribute('type', 'button');
-		button.setAttribute('id', 'picture-in-picture-button');
-		button.setAttribute('title',
-		t.options.picInPicTitle == null ? mejs.i18n.t('mejs.picture-in-pictureText') : t.options.picInPicTitle);
-	       	buttonWrapper.className = `${t.options.classPrefix}button ${t.options.classPrefix}picture-in-picture-button`;
+				// define inner button attributes
+       	button.setAttribute('type', 'button');
+				button.setAttribute('id', 'picture-in-picture-button');
+				button.setAttribute('title',
+				t.options.picInPicTitle == null ? mejs.i18n.t('mejs.picture-in-pictureText') : t.options.picInPicTitle);
+       	buttonWrapper.className = `${t.options.classPrefix}button ${t.options.classPrefix}picture-in-picture-button`;
 
-	        buttonWrapper.appendChild(button);
+        buttonWrapper.appendChild(button);
 
-		// for more info https://developer.apple.com/documentation/webkitjs/adding_picture_in_picture_to_your_safari_media_controls?language=javascript
-	        if (video.webkitSupportsPresentationMode && typeof video.webkitSetPresentationMode === "function") {
-	        	// Toggle PiP when the user clicks the button.
-	        	button.addEventListener("click", function(event) {
-	        		video.webkitSetPresentationMode(video.webkitPresentationMode === "picture-in-picture" ? "inline" : "picture-in-picture");
-	        	});
-	        } else {
-	        	return;
-	        }
+				// for more info https://developer.apple.com/documentation/webkitjs/adding_picture_in_picture_to_your_safari_media_controls?language=javascript
+        if (video.webkitSupportsPresentationMode && typeof video.webkitSetPresentationMode === "function") {
+        	// Toggle PiP when the user clicks the button.
+        	button.addEventListener("click", function(event) {
+        		video.webkitSetPresentationMode(video.webkitPresentationMode === "picture-in-picture" ? "inline" : "picture-in-picture");
+        	});
+        } else {
+        	return;
+        }
 
-          	if(t.options.picInPicScaleStart) {
-			// makes sure that this is a onetime toggle
-			let changedMode = false;
-            		video.addEventListener('play', function() {
-					if(!changedMode) {
-						video.webkitSetPresentationMode("picture-in-picture");
-						changedMode = true;
-					}
-            		});
-          	}
+      	if(t.options.picInPicScaleStart) {
+					// makes sure that this is a onetime toggle
+					let changedMode = false;
+        	video.addEventListener('play', function() {
+						if(!changedMode) {
+							video.webkitSetPresentationMode("picture-in-picture");
+							changedMode = true;
+						}
+        	});
+      	}
 
-          	if(t.options.standartScaleEnd) {
-			video.addEventListener('ended', function () {
-              			video.webkitSetPresentationMode("inline");
-			});
-          	}
-
-	        t.addControlElement(button, 'pictureInPicture');
-		}
-
-    }
+      	if(t.options.standartScaleEnd) {
+					video.addEventListener('ended', function () {
+          	video.webkitSetPresentationMode("inline");
+					});
+      	}
+      	t.addControlElement(button, 'pictureInPicture');
+			}
+  	}
 });
