0fc2c05f716ac73ca414b8620625c2e06b7ac54a
Ogün Karakuş <kirk5bucuk@gmail.com>
2019-08-18 14:45:54 +0300

    Fix

--- src/ads/ads-i18n.js
+++ src/ads/ads-i18n.js
@@ -77,8 +77,8 @@ if (mejs.i18n.sv !== undefined) {
 	mejs.i18n.sv['mejs.ad-skip-info'] = ['Hoppa in 1 second', 'Hoppa in %1 sekunder'];
 }
 if (mejs.i18n.tr !== undefined) {
-    mejs.i18n.tr['mejs.ad-skip'] = 'Werbung überspringen';
-    mejs.i18n.tr['mejs.ad-skip-info'] = ['Überspringen in 1 Sekunde', 'Überspringen in %1 Sekunden'];
+    mejs.i18n.tr['mejs.ad-skip'] = 'Reklamı atla';
+    mejs.i18n.tr['mejs.ad-skip-info'] = ['1 saniye sonra atla', '%1 saniye sonra atla'];
 }
 if (mejs.i18n.uk !== undefined) {
 	mejs.i18n.uk['mejs.ad-skip'] = 'Пропустити рекламу';
