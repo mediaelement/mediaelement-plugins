32a58c6d3903032a69f2bb7441f9823c9edb282e
berryamin <37262984+berryamin@users.noreply.github.com>
2020-03-09 15:30:21 +0100

    fix identation

--- src/source-chooser/source-chooser-i18n.js
+++ src/source-chooser/source-chooser-i18n.js
@@ -58,7 +58,7 @@ if (mejs.i18n.sv !== undefined) {
 	mejs.i18n.sv['mejs.source-chooser']= 'Välj källa';
 }
 if (mejs.i18n.tr !== undefined) {
-    mejs.i18n.tr['mejs.source-chooser']= 'Kaynak Seçici';
+	mejs.i18n.tr['mejs.source-chooser']= 'Kaynak Seçici';
 }
 if (mejs.i18n.uk !== undefined) {
 	mejs.i18n.uk['mejs.source-chooser']= 'Вибір джерела';
