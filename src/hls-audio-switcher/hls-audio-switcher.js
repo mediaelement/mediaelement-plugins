3218d3727b5b804a655f5f95e1d3ae5d61a6da78
Daniel Morgenstern <daniel.morgenstern@init.de>
2021-04-11 14:50:43 +0200

    Add audio switch icon to markup

--- src/hls-audio-switcher/hls-audio-switcher.js
+++ src/hls-audio-switcher/hls-audio-switcher.js
@@ -1,6 +1,7 @@
 "use strict";
 
-const dummyLabel = "switch";
+const icon =  `<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5h12M9 3v2m1.048 9.5A18.022 18.022 0 016.412 9m6.088 9h7M11 21l5-10 5 10M12.751 5C11.783 10.77 8.07 15.61 3 18.129"></path></svg>`;
+const dummyLabel = "switch audio";
 
 /**
  * [Name of feature]
@@ -58,7 +59,7 @@ Object.assign(MediaElementPlayer.prototype, {
 
     const button = document.createElement("div");
     button.className = `${classPrefix}button ${classPrefix}hls-audio-switcher-button`;
-    button.innerHTML = `<button type="button" aria-controls="${this.id}" title="${dummyLabel}" aria-label="${dummyLabel}" tabindex="0">${dummyLabel}</button>`;
+    button.innerHTML = `<button type="button" aria-controls="${this.id}" title="${dummyLabel}" aria-label="${dummyLabel}" tabindex="0">${icon}</button>`;
 
     const popover = document.createElement("div");
     const ul = document.createElement("ul");
